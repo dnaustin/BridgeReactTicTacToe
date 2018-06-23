19a798a3a054a3a1c470c999c6af3d4e0d498a1b
Dave Austin <dave-austin@live.co.uk>
2018-06-23 20:58:24 +0100

    Update AssemblyInfo

---
+++ BridgeReactTicTacToe/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bridge.React Tic-Tac-Toe")]
+[assembly: AssemblyDescription("Implementation of Reactjs.org Tic-Tac-Toe tutorial using Bridge.React")]
+[assembly: AssemblyProduct("Bridge.React Tic-Tac-Toe")]
+[assembly: AssemblyCopyright("Copyright © dnaustin 2018")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58635e8b-5ae4-497f-bc5d-d4a8584bf2a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
